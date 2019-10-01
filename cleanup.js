0e872b9306eb586edef036ff0e6d00ef53114799
Alex Saveliev <alexvsaveliev@gmail.com>
2019-10-01 14:32:53 -0400

    optimized

--- cleanup.js
+++ cleanup.js
@@ -1,24 +1,23 @@
 // Object to capture process exits and call app specific cleanup function
-function noOp() {};
+const noOp = () => {};
 
-exports.Cleanup = function Cleanup(callback) {
+exports.Cleanup = (callback = noOp()) => {
   // attach user callback to the process event emitter
   // if no callback, it will still exit gracefully on Ctrl-C
-  callback = callback || noOp;
   process.on('cleanup',callback);
 
   // do app specific cleaning before exiting
-  process.on('exit', function () {
-    process.emit('cleanup');
-  });
+  process.on('exit', () =>
+    process.emit('cleanup')
+  );
 
   // catch ctrl+c event and exit normally
-  process.on('SIGINT', function () {
-    process.exit(2);
-  });
+  process.on('SIGINT', () =>
+    process.exit(2)
+  );
 
   //catch uncaught exceptions, trace, then exit normally
-  process.on('uncaughtException', function(e) {
-    process.exit(99);
-  });
+  process.on('uncaughtException', () =>
+    process.exit(99)
+  );
 };
