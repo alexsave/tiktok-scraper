b967b4f6afed8627665507ad3f2ebfa05f264485
Alex Saveliev <alexvsaveliev@gmail.com>
2019-10-02 22:46:17 -0400

    got chartjs

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,16 @@
-class App extends React.Component{
+import React, {Component} from 'react';
+import BaseGraph from "./components/BaseGraph";
 
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return <div>
+      <BaseGraph data={{}}/>
+    </div>
+  }
 }
 
 export default App;
