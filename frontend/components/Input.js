8cba984c3a7da85251ccc6f90c2db0e14efdcd42
Alex Saveliev <alexvsaveliev@gmail.com>
2019-10-03 00:25:20 -0400

    styling

--- frontend/components/Input.js
+++ frontend/components/Input.js
@@ -8,20 +8,13 @@ class Input extends Component{
     this.state = {
       username: '',
     };
-    this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(callback){
-    console.log(callback);
-    console.log(this.state.username);
   }
 
   render(){
     return <div>
       <input onChange={e => this.setState({username: e.target.value})}/>
-      <DataConsumer>{
-          context => <button onClick={() => context.fetchData(this.state.username)}/>
-        }
+      <DataConsumer>
+        { context => <button onClick={() => context.fetchData(this.state.username)}/> }
       </DataConsumer>
     </div>
   };
