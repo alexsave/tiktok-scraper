1fe21dd724dde57410e30fa39c7d80df77076ac4
Alex Saveliev <alexvsaveliev@gmail.com>
2019-10-03 14:45:40 -0400

    i think time is working now

--- frontend/util/Conversion.js
+++ frontend/util/Conversion.js
@@ -1,6 +1,3 @@
-//setFullYear(2000)
-//setMonth(0)
-//setDate(1)
 export const UnixToSeconds = unix => {
   let dt = new Date(unix);
   let now = new Date();
@@ -8,10 +5,4 @@ export const UnixToSeconds = unix => {
   now.setMinutes(dt.getMinutes());
   now.setSeconds(dt.getSeconds());
   return now.getTime();
-
-  let res = (dt.getUTCSeconds() + 60* dt.getUTCMinutes() + 60*60 *dt.getUTCHours())*1000;
-  //this is due to gmt vs est
-  if(res < 5*60*60*1000)
-    res += 24*60*60*1000;
-  return res;
 };
