d3aa91c3e740d93113a6450232c2900a0eac50d0
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-19 14:04:29 -0500

    updated packages

--- frontend/components/BaseGraph.js
+++ frontend/components/BaseGraph.js
@@ -33,7 +33,6 @@ class BaseGraph extends Component{
         <DataConsumer>
           {context => {
             //to get rid of musical.ly shit
-            console.log(context.getData);
             const tiktok = context.getData;//.filter(vid => vid.time > new Date('2 August 2018').getTime());
             const cutted = Cut(tiktok, context.getMinLikes);
             const rounded = RoundTime(cutted, context.getTimeIncrement);
