abdd4e0574803c305036baed85c705902faf8b7b
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-28 19:54:01 -0500

    added recursive callback thign

--- backend/video2.js
+++ backend/video2.js
@@ -10,13 +10,12 @@ let vids = [];
 
 let map;
 const FILE = './data.json';
+const headers = {'Accept-Encoding': 'gzip', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0'};
 
-async function downloadVid(page, url){
-  const options = {
-    headers: {'Accept-Encoding': 'gzip', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0'}
-  };
+async function downloadVid(url){
+
   const buffer = [];
-  https.get(url, options, res => {
+  https.get(url, {headers}, res => {
     const gunzip = zlib.createGunzip();
     res.pipe(gunzip);
     gunzip.on('data', data => buffer.push(data.toString()));
@@ -36,30 +35,51 @@ async function downloadVid(page, url){
   await page.waitFor(500);
 }
 
-async function run(username){
+const downloadVids = (urls, cb) => {
+  if(urls.length === 0)
+    cb();
+
+  const url = urls.pop();
+  const buffer = [];
+  https.get(url, {headers}, res => {
+    const gunzip = zlib.createGunzip();
+    res.pipe(gunzip);
+    gunzip.on('data', data => buffer.push(data.toString()));
+    gunzip.on('end', () => {
+
+      downloadVids(urls, cb);
+    });
+  });
+};
+
+function run(username){
   const userdata = map[username];
   if(!userdata)
     console.log('user not available');
   execSync('rm -rf videos && mkdir videos');
   execSync(`rm -f ${username}.mp4`);
   // Set up browser and page.
-  const browser = await puppeteer.launch({
+  /*const browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
-  await page.setViewport({width: 1280, height: 926});
+  await page.setViewport({width: 1280, height: 926});*/
 
   const ids = Object.keys(userdata.tiktoks);
   let urls = ids.map(id => `https://www.tiktok.com/@${username}/video/${id}`);
-  urls = urls.reverse();
+  //urls = urls.reverse();
   //for(let url of urls)
   //await downloadVid(page, url);
-  await downloadVid(page, urls[0]);
+  //await downloadVid(urls[0]);
+  downloadVids(urls, composeVideos);
 
   // Close the browser.
-  await browser.close();
+  //await browser.close();
 
+}
+
+const composeVideos = () => {
   //video composition
   for(let vid of vids)
     execSync(`ffmpeg -i ${vid} -c copy -bsf:v h264_mp4toannexb ${vid}.ts`);
@@ -69,7 +89,8 @@ async function run(username){
   execSync(`ffmpeg -i "concat:${something}" -c copy -absf aac_adtstoasc ${username}.mp4`);
 
   execSync('rm -rf videos/');
-}
+
+};
 
 let raw = fs.readFileSync(FILE);
 map = JSON.parse(raw);
